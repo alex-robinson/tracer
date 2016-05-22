82ab2a86212354c9b4b6dff3023a541a53464291
Alexander Robinson <robinson@fis.ucm.es>
2016-05-22 10:22:57 +0200

    Added deposition object and various characteristics of interest (elevation, position, climate, etc.).

--- src/tracer.f90
+++ src/tracer.f90
@@ -17,21 +17,39 @@ module tracer
 
     type tracer_par_class 
         integer :: n, n_active
-        real(prec) :: time_now, time_old, dt  
+        real(prec) :: time_now, time_old, dt 
+        real(prec) :: thk_lim                   ! Minimum thickness of tracer (m)
+        real(prec) :: z_lim                     ! Minimum depth of tracer (fraction)
     end type 
 
     type tracer_state_class 
         logical, allocatable :: is_active(:) 
         real(prec), allocatable :: x(:), y(:), z(:)
         real(prec), allocatable :: u(:), v(:), w(:)
-        real(prec), allocatable :: time_dep(:) 
+        real(prec), allocatable :: thk(:)            ! Tracer thickness (for compression)
+    end type 
+
+    type tracer_dep_class 
+        ! Standard deposition information (time and place)
+        real(prec), allocatable :: time(:) 
+        real(prec), allocatable :: elev(:) 
+        real(prec), allocatable :: x(:), y(:)
+        real(prec), allocatable :: lon(:), lat(:) 
+
+        ! Additional tracer deposition information (climate, isotopes, etc)
+        real(prec), allocatable :: t2m(:,:)     ! 4 seasons 
+        real(prec), allocatable :: pr(:,:)      ! 4 seasons 
+        real(prec), allocatable :: t2m_prann(:) ! Precip-weighted temp
+
+!         real(prec), allocatable :: d18O(:)
+!         real(prec), allocatable :: dD(:)
 
     end type 
 
     type tracer_class 
         type(tracer_par_class)   :: par 
         type(tracer_state_class) :: now 
-
+        type(tracer_dep_class)   :: dep 
     end type 
 
     private 
@@ -41,41 +59,41 @@ module tracer
 
 contains 
 
-    subroutine tracer_init(par,now)
+    subroutine tracer_init(trc)
 
         implicit none 
 
-        type(tracer_par_class),   intent(OUT) :: par 
-        type(tracer_state_class), intent(OUT) :: now 
-
+        type(tracer_class),   intent(OUT) :: trc 
+        
         ! Load the parameters
-        call tracer_par_load(par)
+        call tracer_par_load(trc%par)
 
         ! Allocate the state variables 
-        call tracer_allocate(now,n=par%n)
+        call tracer_allocate(trc%now,trc%dep,n=trc%par%n)
 
         ! Initialize state 
-        now%is_active = .FALSE. 
+        trc%now%is_active = .FALSE. 
 
-        now%x         = 0.0 
-        now%y         = 0.0 
-        now%z         = 0.0 
-        now%u         = 0.0 
-        now%v         = 0.0 
-        now%w         = 0.0 
-        now%time_dep  = 0.0 
+        trc%now%x         = 0.0 
+        trc%now%y         = 0.0 
+        trc%now%z         = 0.0 
+        trc%now%u         = 0.0 
+        trc%now%v         = 0.0 
+        trc%now%w         = 0.0 
+        trc%now%thk       = 0.0 
 
         return 
 
     end subroutine tracer_init
 
-    subroutine tracer_update(par,now,time,x0,y0,z0,u0,v0,w0)
+    subroutine tracer_update(par,now,dep,time,x0,y0,z0,u0,v0,w0)
 
         implicit none 
 
         type(tracer_par_class),   intent(INOUT) :: par 
         type(tracer_state_class), intent(INOUT) :: now 
-        real(prec), intent(IN) :: time 
+        type(tracer_dep_class),   intent(INOUT) :: dep
+            real(prec), intent(IN) :: time 
         real(prec), intent(IN) :: x0(:,:,:), y0(:,:,:), z0(:,:,:)
         real(prec), intent(IN) :: u0(:,:,:), v0(:,:,:), w0(:,:,:)
         
@@ -83,12 +101,6 @@ contains
         par%time_old = par%time_now 
         par%time_now = time 
         par%dt       = par%time_now - par%time_old 
-
-        ! Deposit new tracer points
-
-        ! == TO DO == 
-        ! - Function based on H and U for location, input par deposition frequency 
-        ! - Attach whatever information we want to trace (age, climate, isotopes, etc)
 
         ! Interpolate velocities to active point locations 
 
@@ -104,6 +116,13 @@ contains
         ! Destroy points that moved outside the valid region 
 
         ! == TO DO == 
+
+        ! Finally, deposit new tracer points
+
+        ! == TO DO == 
+        ! - Generate position based on a random algorithm plus weighting
+        ! - Weighting function based on H and U for location, input par deposition frequency 
+        ! - Attach whatever information we want to trace (age, deposition elevation and location, climate, isotopes, etc)
 
 
 
@@ -179,39 +198,48 @@ contains
         par%time_old = 0.0 
         par%dt       = 0.0 
 
+        par%thk_lim  = 1e-2  ! m 
+        par%z_lim    = 0.99  ! fraction of thickness
+
+
         return 
 
     end subroutine tracer_par_load
 
-    subroutine tracer_allocate(now,n)
+    subroutine tracer_allocate(now,dep,n)
 
         implicit none 
 
         type(tracer_state_class), intent(INOUT) :: now 
+        type(tracer_dep_class),   intent(INOUT) :: dep
         integer, intent(IN) :: n
 
         ! Make object is deallocated
-        call tracer_deallocate(now)
+        call tracer_deallocate(now,dep)
 
         ! Allocate tracer 
         allocate(now%is_active(n))
         allocate(now%x(n),now%y(n),now%z(n))
         allocate(now%u(n),now%v(n),now%w(n))
+        allocate(now%thk(n))
 
-        ! Allocate tracer properties 
-        allocate(now%time_dep(n))
-
+        ! Allocate deposition properties 
+        allocate(dep%time(n), dep%elev(n))
+        allocate(dep%x(n), dep%y(n), dep%lon(n), dep%lat(n))
+        allocate(dep%t2m(4,n), dep%pr(4,n), dep%t2m_prann(n))
+        
         return
 
     end subroutine tracer_allocate
 
-    subroutine tracer_deallocate(now)
+    subroutine tracer_deallocate(now,dep)
 
         implicit none 
 
         type(tracer_state_class), intent(INOUT) :: now 
-
-        ! Allocate state objects
+        type(tracer_dep_class),   intent(INOUT) :: dep
+        
+        ! Deallocate state objects
         if (allocated(now%is_active)) deallocate(now%is_active)
         if (allocated(now%x))         deallocate(now%x)
         if (allocated(now%y))         deallocate(now%y)
@@ -219,9 +247,19 @@ contains
         if (allocated(now%u))         deallocate(now%u)
         if (allocated(now%v))         deallocate(now%v)
         if (allocated(now%w))         deallocate(now%w)
+        if (allocated(now%thk))       deallocate(now%thk)
 
-        if (allocated(now%time_dep))  deallocate(now%time_dep)
-
+        ! Deallocate deposition objects
+        if (allocated(dep%time))      deallocate(dep%time)
+        if (allocated(dep%elev))      deallocate(dep%elev)
+        if (allocated(dep%x))         deallocate(dep%x)
+        if (allocated(dep%y))         deallocate(dep%y)
+        if (allocated(dep%lon))       deallocate(dep%lon)
+        if (allocated(dep%lat))       deallocate(dep%lat)
+        if (allocated(dep%t2m))       deallocate(dep%t2m)
+        if (allocated(dep%pr))        deallocate(dep%pr)
+        if (allocated(dep%t2m_prann)) deallocate(dep%t2m_prann)
+        
         return
 
     end subroutine tracer_deallocate
